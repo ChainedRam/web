e0fbb0503c647424322190cd902a291ff175e542
Khaled Alhendi <kld0093@hotmail.com>
2018-07-01 18:49:59 +0300

    Bootstraped Contact page

--- ReactApp/ClientApp/components/Contact.tsx
+++ ReactApp/ClientApp/components/Contact.tsx
@@ -1,18 +1,21 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Panel, PanelBody } from 'react-bootstrap';
 
 export default class Contact extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
-            <h2>Contact us:</h2>
-            <p>you can reach us at:</p>
-
-            <ul>
-                <li> Our Email: contact@chainedram.com</li>
-                <li><a href="https://twitter.com/ChainedRam">Twitter</a></li>
-                <li><a href="https://www.facebook.com/ChainedRam/">Facebook</a></li>
-                <li><a href="https://discord.gg/b2pgr9E">Discord</a></li>
-            </ul>
+            <Panel>
+                <Panel.Heading>Contact us:</Panel.Heading>
+                <Panel.Body>
+                <ul>
+                    <li> Email: contact@chainedram.com</li>
+                    <li><a href="https://twitter.com/ChainedRam">Twitter</a></li>
+                    <li><a href="https://www.facebook.com/ChainedRam/">Facebook</a></li>
+                    <li><a href="https://discord.gg/b2pgr9E">Discord</a></li>
+                    </ul>
+                </Panel.Body>
+            </Panel>
         </div>;
     }
 }

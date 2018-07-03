4831b86ac8969dfef406b63ea7559d1a8130eff5
Khaled Alhendi <kld0093@hotmail.com>
2018-07-03 22:56:33 +0300

    Connected Projects view to  redux

--- ReactApp/ClientApp/components/Projects.tsx
+++ ReactApp/ClientApp/components/Projects.tsx
@@ -1,43 +1,51 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { Panel } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { ApplicationState } from 'ClientApp/store';
+import * as Ram from '../store/Ram';
 
-export default class Projects extends React.Component<RouteComponentProps<{}>, {}> {
+type ProjectsProps =
+    Ram.RamState      // ... state we've requested from the Redux store
+    & typeof Ram.actionCreators      // ... plus action creators we've requested
+    & RouteComponentProps<{}>; // ... plus incoming routing parameters
+
+
+class Projects extends React.Component<ProjectsProps, {}> {
+
+    componentWillMount() {
+        this.props.requestProjects(); 
+    }
+
+
     public render() {
         return <div>
-            <h2><strong> Projects:</strong></h2>
-            <h3>Current:</h3>
+            <Panel>
+                <Panel.Heading><h3>Current Projects</h3></Panel.Heading>
+                <Panel.Body>
 
-            <h4>Alebi</h4>
-            <p> Alebi is a short puzzle game where you play as Naga, the camel, as she tried to get back her stolen delicious milk!</p>
+                    {this.props.projects.map((p,i) =>
+                        <div key={i}>
+                        <h4>{p.title}</h4>
+                        <p>{p.description}</p>
 
-            <h5>Members:</h5>
-            <ul>
-                <li>Lead Design: Montring(Our token black guy)</li>
-                <li> Lead developer: KLD</li>
-                <li>Writer: Dragim</li>
-            </ul>
-            <h5><em>Release date:</em></h5>
-            <p> Jan 2020</p>
-
-            <h4>Rambot</h4>
-            <p>Interactive multi platform chat bot that can perform numerous textual and vocal commands</p>
-            <h5>Members:</h5>
-            <ul>
-                <li> Lead developer: KLD</li>
-            </ul>
-            <h5><em>Release date:</em></h5>
-            <p> Feb 2020</p>
-
-
-            <h3>Future:</h3>
-
-            <h4>Sophia</h4>
-            <p> This is a story about a robot that attempts to achieve it's goal about making every human life better, by exterminating humans </p>
-            <h5>Members:</h5>
-            <ul>
-                <li>Writer: Qanobi</li>
-                <li>Editor: KLD</li>
-            </ul>
+                        <h5>Members:</h5>
+                        <ul>
+                            {p.members.map((m,i) =>
+                                <li key={i}>{m.role}: {m.name}</li>
+                                )}
+                        </ul>
+                        <h5><em>Release date:</em></h5>
+                        <p>{p.releaseDate}</p>
+                    </div>
+                    )}
+                </Panel.Body>
+            </Panel>
         </div>;
     }
 }
+
+export default connect(
+    (state: ApplicationState) => state.ram, // Selects which state properties are merged into the component's props
+    Ram.actionCreators                 // Selects which action creators are merged into the component's props
+)(Projects as any) as any;
